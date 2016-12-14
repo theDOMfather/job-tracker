cc797f533cc755b28f5da13e2fee93ba6ed0bbfc
Collin Tharp <catharp@gmail.com>
2016-12-13 16:22:41 -0800

    [MODIFY](db): switch to DOMfather db {CT}

--- server/db.js
+++ server/db.js
@@ -4,6 +4,6 @@ var config = require('../config.js');
 var USER = process.env.DB_USER || config.db.user;
 var PASSWORD = process.env.DB_PASSWORD || config.db.password;
 
-mongoose.connect(`mongodb://${USER}:${PASSWORD}@ds115738.mlab.com:15738/charlottes_webpack`);
+mongoose.connect(`mongodb://${USER}:${PASSWORD}@ds133418.mlab.com:33418/heroku_2thfdbfj`);
 
 module.exports = mongoose.connection;
