e102ab3bd53fc035f90d946eb577172f00d6fc74
Collin Tharp <catharp@gmail.com>
2016-12-16 15:43:36 -0800

    [ADD](server): imlement request to kairos facial recognition api on signup {CT}

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 var User = require('../models/userModel.js');
+var config = require('../../config.js');
+var http = require('http');
 
 module.exports = {
   addUser: function(userObj) {
@@ -7,6 +9,33 @@ module.exports = {
         if(err) {
           reject(err);
         }else {
+          let options = {
+            method: 'POST',
+            host: 'api.kairos.com',
+            path: '/enroll',
+            headers: {
+              'Content-Type': 'application/json',
+              'app_id': config.kairos.id,
+              'app_key': config.kairos.key
+            }
+          };
+          let data = {
+            'image': userObj.image,
+            'subject_id': userObj.username,
+            'gallery_name': 'users'
+          };
+          console.log(options);
+          console.log(data);
+
+          let req = http.request(options, function(res) {
+            let body = '';
+            res.on('data', chunk => body += chunk);
+            res.on('end', () => console.log(body));
+          });
+          req.write(JSON.stringify(data));
+          console.log('req.data: ', req.data);
+          req.end();
+
           resolve(newUser);
         }
       });
@@ -26,7 +55,7 @@ module.exports = {
   },
 
   updateUserInDb: function(user, userId) {
-    return User.update({"_id": userId}, user)
+    return User.update({'_id': userId}, user)
     .exec()
     .then(function(resp) {
       return resp;
@@ -41,7 +70,7 @@ module.exports = {
 
   deleteUser: function(userId) {
     return User.remove(
-      {"_id": userId}
+      {'_id': userId}
       )
     .exec()
     .then(function(resp) {
