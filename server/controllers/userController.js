bb66ea7565039eef283a4e4f98e43663759a0ff3
Collin Tharp <catharp@gmail.com>
2016-12-16 16:22:34 -0800

    [MODIFY](server): split facial recognition onto dedicated route {CT}

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,36 +9,37 @@ module.exports = {
         if(err) {
           reject(err);
         }else {
-          let options = {
-            method: 'POST',
-            host: 'api.kairos.com',
-            path: '/enroll',
-            headers: {
-              'Content-Type': 'application/json',
-              'app_id': config.kairos.id,
-              'app_key': config.kairos.key
-            }
-          };
-          let data = {
-            'image': userObj.image,
-            'subject_id': userObj.username,
-            'gallery_name': 'users'
-          };
-          console.log(options);
-          console.log(data);
-
-          let req = http.request(options, function(res) {
-            let body = '';
-            res.on('data', chunk => body += chunk);
-            res.on('end', () => console.log(body));
-          });
-          req.write(JSON.stringify(data));
-          console.log('req.data: ', req.data);
-          req.end();
-
           resolve(newUser);
         }
       });
+    });
+  },
+
+  enrollPhoto: function(userId, userPhoto) {
+    return new Promise(function(resolve, reject) {
+      let options = {
+        method: 'POST',
+        host: 'api.kairos.com',
+        path: '/enroll',
+        headers: {
+          'Content-Type': 'application/json',
+          'app_id': config.kairos.id,
+          'app_key': config.kairos.key
+        }
+      };
+      let data = {
+        'image': userPhoto,
+        'subject_id': userId,
+        'gallery_name': 'users'
+      };
+      let req = http.request(options, function(res) {
+        let body = '';
+        res.on('data', chunk => body += chunk);
+        res.on('end', () => resolve(body));
+      });
+      req.write(JSON.stringify(data));
+      req.on('error', err => reject(err));
+      req.end();
     });
   },
 
